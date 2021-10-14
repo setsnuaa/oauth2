e233b570819499175ce498797dc09f1165c1329e
setsnuaa <1051338166@qq.com>
2021-10-14 20:29:02 +0800

    UPGRADE

--- src/main/java/com/uestc/oauth2/config/LogoutSuccessConfig.java
+++ src/main/java/com/uestc/oauth2/config/LogoutSuccessConfig.java
@@ -2,11 +2,9 @@ package com.uestc.oauth2.config;
 
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-
 /**
  * @name:
  * @author:Setsnua
@@ -18,11 +16,17 @@ public class LogoutSuccessConfig {
     @Resource
     private ConsumerTokenServices consumerTokenServices;
 
+    /**
+     * 退出登录并删除redis中的token
+     * @param accessToken
+     * @return
+     */
     @GetMapping("/oauth/logout")
-    public Object logout(@RequestParam("token") String accessToken) {
+    public Object logout(String accessToken) {
         if (consumerTokenServices.revokeToken(accessToken)) {
             return LogoutResult.ok();
         }
         return LogoutResult.fail();
     }
+
 }
